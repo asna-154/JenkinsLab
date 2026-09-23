e437162450e319146964a2ba2f91666baea081fe
Asna Hammad <asnahammad11@gmail.com>
2026-09-23 12:55:03 +0500

    Fix typo in output message in HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,6 +1,6 @@
 public class HelloJenkins{
   public static void main(String[] args){
     System.out.print("Hello from jenkins version2");
-    System.out.println("Githu Successful");
+    System.out.println("Githu Successful")
 }
 }
