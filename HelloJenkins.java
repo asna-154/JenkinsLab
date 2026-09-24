086a1fcc937771d6a4677ce4e8f36a2abb3b68d8
Asna Hammad <asnahammad11@gmail.com>
2026-09-24 22:26:25 +0500

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,6 +1,6 @@
 public class HelloJenkins{
   public static void main(String[] args){
     System.out.print("Hello from jenkins version");
-    System.out.println("Github Successful");
+    System.out.println("Github is Successful");
 }
 }
